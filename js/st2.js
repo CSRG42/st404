f38c557a67c25bf2644154ccb8a4a59b10c954dd
CSRG42 <86738740+CSRG42@users.noreply.github.com>
2021-07-11 02:05:47 -0300

    Delete js directory

--- js/st2.js
+++
@@ -1,9 +0,0 @@
-var LS = LS || {};
-
-// Making it a thenable so it can be made into a full fledged Promise later
-LS._readyCallbacks = [];
-LS.ready = {
-    then: function (callback) {
-        LS._readyCallbacks.push(callback);
-    },
-};
