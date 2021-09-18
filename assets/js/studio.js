c65ddace69bbe4a2d9d09772cb0fd25a4ac29602
CSRG42 <86738740+CSRG42@users.noreply.github.com>
2021-09-18 20:04:58 -0300

    Add files via upload

--- assets/js/studio.js
+++ assets/js/studio.js
@@ -5,7 +5,7 @@ if (window.location.href === "https://www.studio404.com.br/") {
     $ ('.ocultar').show(); 
 } else if (window.location.href === "https://studio404.com.br/") {
     $ ('.ocultar').show()
-} else if (/^\?fbclid=/.test(location.search)) {
+} else if (location.search) {
     location.replace(location.href.replace(/\?fbclid.+/, ""))
 } else {
     $ ('.ocultar').hide();
