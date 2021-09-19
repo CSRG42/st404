90b4275ad40e27b31a1d22a4ac46fefa0ca5b001
CSRG42 <86738740+CSRG42@users.noreply.github.com>
2021-09-19 00:10:20 -0300

    Add files via upload

--- assets/js/studio.js
+++ assets/js/studio.js
@@ -1,13 +1,7 @@
 /*
  jQuery Show / Hide Div com base no parâmetro de URL 
  */
-if (window.location.href === "https://www.studio404.com.br/") {
-    $ ('.ocultar').show(); 
-} else if (window.location.href === "https://studio404.com.br/") {
-    $ ('.ocultar').show()
-} else {
-    $ ('.ocultar').hide();
-} 
+
 
 !function(){for(var e,n=function(){},o=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],i=o.length,r=window.console=window.console||{};i--;)r[e=o[i]]||(r[e]=n)}();
 
