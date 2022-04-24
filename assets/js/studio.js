6c3f83965a3696880cc1bd80bf2ea1a8bfa43eae
CSRG42 <86738740+CSRG42@users.noreply.github.com>
2022-04-24 03:57:28 -0300

    Add files via upload

--- assets/js/studio.js
+++ assets/js/studio.js
@@ -12,63 +12,6 @@ if (window.location.href === "https://www.studio404.com.br/") {
 }  */
 
 
-/*
- LGPD 
-*/
-function pureFadeIn(e, o) {
-    var i = document.getElementById(e);
-    i.style.opacity = 0, i.style.display = o || "block",
-        function e() {
-            var o = parseFloat(i.style.opacity);
-            (o += .02) > 1 || (i.style.opacity = o, requestAnimationFrame(e))
-        }()
-}
-
-function pureFadeOut(e) {
-    var o = document.getElementById(e);
-    o.style.opacity = 1,
-        function e() {
-            (o.style.opacity -= .02) < 0 ? o.style.display = "none" : requestAnimationFrame(e)
-        }()
-}
-
-function setCookie(e, o, i) {
-    var t = "";
-    if (i) {
-        var n = new Date;
-        n.setTime(n.getTime() + 24 * i * 60 * 60 * 1e3), t = "; expires=" + n.toUTCString()
-    }
-    document.cookie = e + "=" + (o || "") + t + "; path=/"
-}
-
-function getCookie(e) {
-    for (var o = e + "=", i = document.cookie.split(";"), t = 0; t < i.length; t++) {
-        for (var n = i[t];
-            " " == n.charAt(0);) n = n.substring(1, n.length);
-        if (0 == n.indexOf(o)) return n.substring(o.length, n.length)
-    }
-    return null
-}
-
-function eraseCookie(e) {
-    document.cookie = e + "=; Max-Age=-99999999;"
-}
-
-function cookieConsent() {
-    getCookie("purecookieDismiss") || (document.body.innerHTML += '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><a>' + purecookieTitle + '</a></div><div class="cookieDesc"><p>' + purecookieDesc + " " + purecookieLink + '</p></div><div class="cookieButton"><a onClick="purecookieDismiss();">' + purecookieButton + "</a></div></div>", pureFadeIn("cookieConsentContainer"))
-}
-
-function purecookieDismiss() {
-    setCookie("purecookieDismiss", "1", 7), pureFadeOut("cookieConsentContainer")
-}
-window.onload = function() {
-    cookieConsent()
-};
-
-/*
- LGPD END
-*/
-
 !function(){for(var e,n=function(){},o=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],i=o.length,r=window.console=window.console||{};i--;)r[e=o[i]]||(r[e]=n)}();
 
 
